2a565d0cf8e206e609924d090584a6369a1c025e
Saerom Bang <saerombang11@gmail.com>
2020-10-29 21:12:59 +0900

    feat: Add jobs page component

---
+++ src/components/pages/jobs/jobs.tsx
@@ -0,0 +1,59 @@
+import { ThemeProvider } from 'emotion-theming';
+import React from 'react';
+import { theme } from '../../../styles/theme';
+import { Cell, Grid } from '../../content/layout-grid/layout-grid';
+import HiringBox from '../../instance/hiring-box/hiring-box';
+import Div from '../../styled-system/div/div';
+import JobsBox from '../../instance/jobs-box/jobs-box';
+import styled from '@emotion/styled';
+import Main from '../../styled-system/main/main';
+import FilterBox from '../../instance/filter-box/filter-box';
+import SubscribeBox from '../../instance/subscribe-box/subscribe-box';
+
+const StyledMain = styled(Main)`
+  background-color: #efefef;
+`;
+
+const Jobs = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <header>an example header</header>
+      <StyledMain>
+        <Grid flexDirection={['column-reverse', 'row']}>
+          <Cell width={[1, null, 2 / 3]}>
+            <Div
+              fontSize="20px"
+              fontWeight="600"
+              mb="20px"
+              paddingTop="4px"
+              paddingBottom="4px"
+              fontFamily={theme.fonts.system}
+            >
+              Jobs
+            </Div>
+            <JobsBox mb="20px" />
+            <JobsBox mb="20px" />
+            <JobsBox mb="20px" />
+          </Cell>
+          <Cell width={[1, 0, 1 / 3]} display={[null, 'none', 'block']}>
+            <HiringBox />
+            <Div
+              fontSize="16px"
+              lineHeight="18px"
+              marginBottom="10px"
+              fontWeight="600"
+              fontFamily={theme.fonts.system}
+            >
+              Filters
+            </Div>
+            <FilterBox />
+            <SubscribeBox mt="20px" mb="20px" />
+          </Cell>
+        </Grid>
+      </StyledMain>
+      <footer>an exmaple footer</footer>
+    </ThemeProvider>
+  );
+};
+
+export default Jobs;
