88e6f9f0cb139d5011a97004687f5acb9c652e1b
Saerom Bang <saerombang11@gmail.com>
2020-10-30 15:21:14 +0900

    fix: Resolve font family and margin bottom error in filter box

--- src/components/instance/filter-box/filter-box.tsx
+++ src/components/instance/filter-box/filter-box.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import Div from '../../styled-system/div/div';
 import Label from '../../styled-system/label/label';
 import Input from '../../styled-system/input/input';
+import { theme } from '../../../styles/theme';
 
 const FilterBox = () => {
   return (
-    <Div backgroundColor="#ffffff" padding="20px" borderRadius="5px" boxShadow="#000001">
+    <Div
+      backgroundColor="#ffffff"
+      padding="20px"
+      borderRadius="5px"
+      boxShadow="0 1px 2px 0 rgba(0,0,0,.1)"
+    >
       <Div
         lineHeight="20px"
         fontSize="13px"
@@ -14,27 +20,62 @@ const FilterBox = () => {
         display="flex"
         flexDirection="column"
       >
-        <Label marginBottom="10px" fontWeight="400!important" lineHeight="20px" fontSize="13px">
+        <Label
+          marginBottom="10px"
+          fontWeight="400!important"
+          lineHeight="20px"
+          fontSize="13px"
+          fontFamily={theme.fonts.system}
+        >
           <Input type="checkbox" margin="0px 10px 0px 0px"></Input>
           ⚒️ Engineering
         </Label>
-        <Label marginBottom="10px" fontWeight="400!important" lineHeight="20px" fontSize="13px">
+        <Label
+          marginBottom="10px"
+          fontWeight="400!important"
+          lineHeight="20px"
+          fontSize="13px"
+          fontFamily={theme.fonts.system}
+        >
           <Input type="checkbox" margin="0px 10px 0px 0px"></Input>
           🎨 Design
         </Label>
-        <Label marginBottom="10px" fontWeight="400!important" lineHeight="20px" fontSize="13px">
+        <Label
+          marginBottom="10px"
+          fontWeight="400!important"
+          lineHeight="20px"
+          fontSize="13px"
+          fontFamily={theme.fonts.system}
+        >
           <Input type="checkbox" margin="0px 10px 0px 0px"></Input>
           📢 Marketing
         </Label>
-        <Label marginBottom="10px" fontWeight="400!important" lineHeight="20px" fontSize="13px">
+        <Label
+          marginBottom="10px"
+          fontWeight="400!important"
+          lineHeight="20px"
+          fontSize="13px"
+          fontFamily={theme.fonts.system}
+        >
           <Input type="checkbox" margin="0px 10px 0px 0px"></Input>
           📈 Sales
         </Label>
-        <Label marginBottom="10px" fontWeight="400!important" lineHeight="20px" fontSize="13px">
+        <Label
+          marginBottom="10px"
+          fontWeight="400!important"
+          lineHeight="20px"
+          fontSize="13px"
+          fontFamily={theme.fonts.system}
+        >
           <Input type="checkbox" margin="0px 10px 0px 0px"></Input>
           💖 Customer Support
         </Label>
-        <Label marginBottom="10px" fontWeight="400!important" lineHeight="20px" fontSize="13px">
+        <Label
+          fontWeight="400!important"
+          lineHeight="20px"
+          fontSize="13px"
+          fontFamily={theme.fonts.system}
+        >
           <Input type="checkbox" margin="0px 10px 0px 0px"></Input>
           💎 Product
         </Label>
